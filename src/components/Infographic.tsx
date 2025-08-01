a329b8b405adabb600c09f894493d246e17b209b
Jubal64 <yungworld.pj1@gmail.com>
2025-08-01 22:20:27 +0100

    Auto update

--- src/components/Infographic.tsx
+++ src/components/Infographic.tsx
@@ -18,14 +18,23 @@ export default function RealisticRibbon() {
         viewBox="896 856 1902 603"
         xmlns="http://www.w3.org/2000/svg"
         >
-        <defs>
+            <defs>
             <linearGradient id="ribbonGradient" x1="0%" y1="50%" x2="100%" y2="50%">
-            <stop offset="0%" stop-color="#1a002b" />
-            <stop offset="40%" stop-color="#a9a9a9" />
-            <stop offset="60%" stop-color="#f0f0f0" />
-            <stop offset="100%" stop-color="#1a002b" />
+                {/* Start left leaf (dark) */}
+                <stop offset="0%" stopColor="#1a002b" />
+                {/* Near edge - brighter */}
+                <stop offset="10%" stopColor="#e0e0e0" />
+                {/* Transition back darker toward center */}
+                <stop offset="25%" stopColor="#555" />
+                {/* Deep dark center */}
+                <stop offset="50%" stopColor="#0e0e0e" />
+                {/* Mirror effect right side */}
+                <stop offset="75%" stopColor="#555" />
+                <stop offset="90%" stopColor="#e0e0e0" />
+                <stop offset="100%" stopColor="#1a002b" />
             </linearGradient>
-        </defs>
+            </defs>
+
         <g>
             <path
             d="M2782.13 1327.33 2788.2 1336.34 2787.24 1332.85ZM984.504 858.5 1098.69 858.5 1098.69 1030.07 1108.78 1030.07 1108.78 1031.46 2690.34 1031.46 2690.36 1031.45C2748.43 1031.45 2795.5 1065.76 2795.5 1108.07L2795.5 1362.67 2789.61 1341.41 2795.5 1370.59C2795.5 1418.04 2757.03 1456.5 2709.58 1456.5L2595.4 1456.5 2595.4 1286.06 2585.22 1286.06 2585.22 1284.67 1003.64 1284.67 1003.64 1284.67C945.572 1284.67 898.5 1250.37 898.5 1208.06L898.5 953.456C898.5 964.034 901.442 974.112 906.762 983.278L914.994 992.175 905.337 977.854C900.99 967.575 898.585 956.275 898.585 944.412 898.585 896.964 937.052 858.5 984.504 858.5Z"
