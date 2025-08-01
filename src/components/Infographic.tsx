55825b7598e1845c1af089476f2148e9b5bf6359
Jubal64 <yungworld.pj1@gmail.com>
2025-08-01 17:16:51 +0100

    Auto update

--- src/components/Infographic.tsx
+++ src/components/Infographic.tsx
@@ -1,15 +1,19 @@
-import styles from '@/styles/RealisticRibbon.module.css';
+'use client';
 
-export default function RealisticRibbon() {
+import styles from '@/styles/Infographic.module.css';
+
+export default function RibbonMock() {
   return (
-    <div className={styles.ribbonWrapper}>
-      <div className={styles.leafLeft} />
-      <div className={styles.extensionLeft} />
-      <div className={styles.ribbonBody}>
-        <span className={styles.text}>LOREM IPSUM</span>
+    <div className={styles.ribbonContainer}>
+      <div className={styles.ribbon}>
+      <div className={styles.ribbonTailLeft}></div>
+        <div className={styles.ribbonCapLeft}></div>
+        <div className={styles.ribbonBody}>
+          <span className={styles.text}>LOREM IPSUM</span>
+        </div>
+        <div className={styles.ribbonCapRight}></div>
+        <div className={styles.ribbonTailRight}></div>
       </div>
-      <div className={styles.extensionRight} />
-      <div className={styles.leafRight} />
     </div>
   );
 }
